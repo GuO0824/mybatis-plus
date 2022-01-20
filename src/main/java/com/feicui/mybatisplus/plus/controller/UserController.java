713879dc3e3e08428505ec76d4d120aa352f233e
guojing <1462047022@qq.com>
2022-01-20 15:14:15 +0800

    mybatis-puls

--- src/main/java/com/feicui/mybatisplus/plus/controller/UserController.java
+++ src/main/java/com/feicui/mybatisplus/plus/controller/UserController.java
@@ -3,8 +3,10 @@ package com.feicui.mybatisplus.plus.controller;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.feicui.mybatisplus.Conv;
 import com.feicui.mybatisplus.plus.UserMapper.CgMatInfoDao;
+import com.feicui.mybatisplus.plus.UserMapper.TuserMapper;
 import com.feicui.mybatisplus.plus.contants.LogAnnocation;
 import com.feicui.mybatisplus.plus.contants.UserEnum;
+import com.feicui.mybatisplus.plus.domain.Tuser;
 import com.feicui.mybatisplus.plus.entity.CgMatInfo;
 import com.feicui.mybatisplus.plus.entity.User;
 import com.feicui.mybatisplus.plus.service.UserService;
@@ -32,6 +34,8 @@ public class UserController {
   @Autowired
   private CgMatInfoDao cgMatInfoDao;
 
+  @Autowired
+  TuserMapper tuserMapper;
 
 
   @GetMapping("/mybatis")
@@ -46,12 +50,12 @@ public class UserController {
 
   @GetMapping("/select")
   @ApiOperation(value = "物资表信息查询")
-  public List<CgMatInfo> get(@ApiParam(value = "物资id") @RequestParam("matId")Integer matId,
-                             @ApiParam(value = "name") @RequestParam(value = "name",required = false) Integer name) throws NoSuchFieldException, IllegalAccessException {
+  public List<CgMatInfo> get(@ApiParam(value = "物资id") @RequestParam("matId") Integer matId,
+                             @ApiParam(value = "name") @RequestParam(value = "name", required = false) Integer name) throws NoSuchFieldException, IllegalAccessException {
     List<CgMatInfo> mats = cgMatInfoDao.selectList(new QueryWrapper<CgMatInfo>().eq("mat_id", matId));
-    System.out.println(mats+"-------------------------"+name);
+    System.out.println(mats + "-------------------------" + name);
 
-    CgMatInfo cgMatInfo=new CgMatInfo();
+    CgMatInfo cgMatInfo = new CgMatInfo();
 //    Field mat_id = cgMatInfo.getClass().getDeclaredField("matId");
 //    mat_id.setAccessible(true);//暴力反射
 //    mat_id.set(cgMatInfo,1111L);
@@ -62,11 +66,10 @@ public class UserController {
     return mats;
   }
 
-
-
-
-
-
-
+  @GetMapping("/getUser")
+  @ApiOperation(value = "tUser表的查询")
+  public List<Tuser> getUser() {
+    return tuserMapper.selectList(null);
+  }
 
 }
--- src/main/java/com/feicui/mybatisplus/plus/domain/Tuser.java
+++ src/main/java/com/feicui/mybatisplus/plus/domain/Tuser.java
@@ -1,8 +1,10 @@
 package com.feicui.mybatisplus.plus.domain;
 
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 @Data
+@TableName("t_user")
 public class Tuser {
 
   private String id;
